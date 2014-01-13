d4cce5fc0aac4d8ab2ab5f901c077f4f29d41dc9
Erick Ruiz <erickruiz29@gmail.com>
2014-01-12 18:16:19 -0800

    Many Changes
    created remove_whitespace, grabBuffer.. ect

--- Lab1/read-command.c
+++ Lab1/read-command.c
@@ -13,8 +13,9 @@
 //Global variables
 ////////////////////////////////////////////////////////////////////////
 
-linecount = 1;
-
+int linecount;
+int (*get_byte) (void *);
+void *get_byte_argument;
 
 //////////////////////////////////////////////////////////////////////
 //struct cmd_node
@@ -29,8 +30,79 @@ linecount = 1;
 //};
 //////////////////////////////////////////////////////////////////////
 
-//Check to make sure 
+//Check to make sure statement line is valid
 bool isStatementValid(char* statement);
+
+void
+syntax_error()
+{
+  error(1, 0, "Syntax Error: Line %d", linecount);
+}
+
+void 
+remove_whitespace()
+{
+  if(!feof(get_byte_argument))
+  {
+    char ch = get_byte(get_byte_argument);
+    while(strchr("\n\t ", ch))
+    {
+      if(ch == '\n')
+        line_count++;
+      ch = get_byte(get_byte_argument);
+    }
+    ungetc(ch, get_byte_argument);
+  }
+}
+
+
+//Grab what type of command buffer is and return to create it
+enum command_type
+grabBuffer(char *word_buf)
+{
+  
+  while(!feof(get_byte_argument))
+  {
+
+    //make sure the comment is in the right place
+    char ch = get_byte(get_byte_argument);
+    char de = get_byte(get_byte_argument);
+    if(de == '#' && !strchr("\n\t ", ch))
+     syntax_error();
+    ungetc(de, get_byte_argument);
+
+    //the special characters that make the unique cases
+    switch(ch) 
+    {
+      case '#':
+        de = get_byte(get_byte_argument);
+        //get to end of comment
+        while(de != '\n')
+        {
+            if(de == EOF)
+              return SIMPLE_COMMAND;
+            de = get_byte(get_byte_argument);
+        }
+        return grabBuffer(word_buf);
+
+      case '&':
+        de = get_byte(get_byte_argument);
+        if(de == '&')
+        { 
+          remove_whitespace();
+          return AND_COMMAND;
+        }
+
+      case '\n': line_count++;
+      case ';':
+      case EOF:
+        return SIMPLE_COMMAND;        
+    }
+    strcat(word_buf, ch);
+  }
+  return SEQUENCE_COMMAND;
+}
+
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
